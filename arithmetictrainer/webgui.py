6bc6595cf1664cb1c2605899a951127540750eba
Geronymo Talas <talasg@posteo.de>
2022-02-04 15:40:42 +0100

    handle post and get

--- arithmetictrainer/webgui.py
+++ arithmetictrainer/webgui.py
@@ -1,15 +1,62 @@
-from http.server import HTTPServer
-from http.server import SimpleHTTPRequestHandler
+import sys
+from http import HTTPStatus
+from http.server import ThreadingHTTPServer
+from http.server import SimpleHTTPRequestHandler, BaseHTTPRequestHandler
 from pathlib import Path
+from decimal import Decimal, InvalidOperation
+
+from core import Arithmetictrainer
+from utils import create_taskgenerators_from_file
 
 PORT = 8000
-HTML_DIR = Path(__file__).parent.joinpath('data/html')
-
-class Handler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=HTML_DIR, **kwargs)
+DATA = Path(__file__).parent.joinpath('data')
+HTML_FILE = DATA.joinpath('html/index.html')
 
 
-with HTTPServer(('localhost', PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+taskgen_list = create_taskgenerators_from_file(DATA.joinpath('config'))
+trainer = Arithmetictrainer(taskgen_list)
+
+class Handler(BaseHTTPRequestHandler):
+    def get_html(self, html_file: Path):
+        task = trainer.getTask()
+        if not html_file.is_file():
+            raise ValueError(f'[{html_file}] is not a file.')
+        with open(html_file) as f:
+            html = f.read()
+        html = html.replace(
+                'RESULT_DECIMAL_POINTS', str(task['result_decimal_points']))
+        html = html.replace('TASK', task['task'])
+        return html.encode()
+
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK.value)
+        self.send_header('Content-Type','text/html')
+        self.end_headers()
+        self.wfile.write(self.get_html(HTML_FILE))
+
+    def do_POST(self):
+        content_len = int(self.headers.get('Content-Length'))
+        print('content length ' + str(content_len))
+        data = self.rfile.read(content_len).decode()
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
+        try:
+            # data should have the form 'answer=NUMBER'
+            data = data.split('=')[-1]
+            trainer.answer(Decimal(data))
+        except InvalidOperation:
+            print(f'Could not convert "{data}" to Decimal.')
+
+
+def main():
+    with ThreadingHTTPServer(('localhost', PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("Keyboard interrupt received, exiting.")
+            sys.exit(0)
+
+if __name__ == '__main__':
+    main()
